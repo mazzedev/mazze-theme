408b6d67d05c9fcbe5b56354a435a8c5dab0686b
Vitor Hugo Rodrigues <vitorhugo.ro10@gmail.com>
2020-11-09 22:21:03 -0300

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,8 +10,8 @@
 	<header class="entry-header lg:mt-20">
 		<h1 class="text-4xl md:text-6xl font-bold text-purple-mazze"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 	</header>
-	<div class="entry-content grid grid-cols-1 lg:grid-cols-3 mt-8">
-		<div class="prose md:prose-xl col-span-2 pr-10">
+	<div class="entry-content grid grid-cols-1 <?php if (has_post_thumbnail()) { echo 'lg:grid-cols-3';} ?> mt-8">
+		<div class="prose md:prose-xl col-span-2 <?php if (has_post_thumbnail()) { echo 'max-w-none'; } else { echo 'pr-10'; } ?>">
 			<?php the_content(); ?>
 		</div>
 		<?php if (has_post_thumbnail()) : ?>
