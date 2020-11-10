72161aad936259b4fde625f9b8b97b7c2b0bb1d8
Vitor Hugo Rodrigues <vitorhugo.ro10@gmail.com>
2020-11-09 21:34:37 -0300

    Update page.php

--- page.php
+++ page.php
@@ -7,7 +7,7 @@
 get_header();
 ?>
 
-<main id="main" class="site-main" role="main">
+<main id="main" class="site-main container mx-auto" role="main">
 
 	<?php
 	while ( have_posts() ) : the_post();
